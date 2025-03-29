290f695c48cc75b61ae0e6bda3920f1c11841aee
Rohan Hasabe <rohanhasabe8@gmail.com>
2025-03-29 16:51:52 -0400

    swearJar: init

---
+++ SwearJar/SwearJarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwearJarApp.swift
+//  SwearJar
+//
+//  Created by Rohan Hasabe on 27/03/25.
+//
+
+import SwiftUI
+
+@main
+struct SwearJarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwearJarTests/SwearJarTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwearJarTests.swift
+//  SwearJarTests
+//
+//  Created by Rohan Hasabe on 27/03/25.
+//
+
+import Testing
+@testable import SwearJar
+
+struct SwearJarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
